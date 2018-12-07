fd87a9dc023088beb80ca281d5476a07fd0d9392
李小龙 <>
2018-12-07 23:03:24 +0800

    提交代码

---
+++ iOS实现波浪效果/iOS实现波浪效果/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS实现波浪效果
+//
+//  Created by 李小龙 on 2018/12/7.
+//  Copyright © 2018 李小龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS实现波浪效果/iOS实现波浪效果/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS实现波浪效果
+//
+//  Created by 李小龙 on 2018/12/7.
+//  Copyright © 2018 李小龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
